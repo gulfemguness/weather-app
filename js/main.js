a2b3b6c18d8033e8ef4f9ca2fc73e7d9a1d2d1a0
gulfemguness <72115959+gulfemguness@users.noreply.github.com>
2022-10-24 10:15:32 -0500

    html only

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// displays the 
+//High, Low, Forecast and Humidity        using JavaScript.
+
+// You will possibly need to change the weather data from Kelvin to Fahrenheit using this formula 
+//(Depending on the API endpoint you are accessing)
+
+// (32K − 273.15) × 9/5 + 32 = -402.1°F
+//Please include a weather icon on your project!
+
+
+// API KEY:
+// b6fff33931c04ffe00d7e96c1ad88c7e
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={b6fff33931c04ffe00d7e96c1ad88c7e}
+
+// WITH ZIP CODE
+// https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
+
